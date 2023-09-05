760392163732e5152c5de854b5ac9525fa007f5b
Dac-Digital <144015159+Dac-Digital@users.noreply.github.com>
2023-09-05 14:38:20 +0200

    Create 5.kt

---
+++ 5.kt
@@ -0,0 +1,6 @@
+user@hostname:~/$ kubectl port-forward pod/port-forward 5432:5432
+Forwarding from 127.0.0.1:5432 -> 5432
+Forwarding from [::1]:5432 -> 5432
+Handling connection for 5432
+Handling connection for 5432
+Handling connection for 5432
